eb2cb967558088e878d353c8b08cc370c804b0c8
kimmo <hemakim0215@naver.com>
2026-01-11 12:05:49 +0900

    .

--- RuleForge/TrpgPlayer.cs
+++ RuleForge/TrpgPlayer.cs
@@ -6,5 +6,23 @@ using System.Threading.Tasks;
 
 namespace RuleForge
 {
+    class PlayerClass
+    {
+        public string ClassName { get; set; }
 
+        public PlayerClass(string className)
+        {
+            ClassName = className;
+        }
+    }
+
+    class TrpgPlayer : TrpgActor
+    {
+        // 클래스의 타입은 룰북에서 정의된것을 사용해야하기때문에 enum을 활용하지않는다.
+        public PlayerClass playerClass { get; set; }
+        public TrpgPlayer(string name, string description = "", string className = "") : base(name, description)
+        {
+            playerClass = new PlayerClass(className);
+        }
+    }
 }
