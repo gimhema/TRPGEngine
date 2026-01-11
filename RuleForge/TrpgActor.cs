dd4f7d6553a4706231e1acba63963caaefdbb340
kimmo <hemakim0215@naver.com>
2026-01-11 12:01:17 +0900

    .

--- RuleForge/TrpgActor.cs
+++ RuleForge/TrpgActor.cs
@@ -8,5 +8,16 @@ namespace RuleForge
 {
     class TrpgActor
     {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public TrpgCommon CommonAttributes { get; set; }
+
+        public TrpgActor(string name, string description = "")
+        {
+            Name = name;
+            Description = description;
+            CommonAttributes = new TrpgCommon();
+        }
     }
 }
--- RuleForge/TrpgCommon.cs
+++ RuleForge/TrpgCommon.cs
@@ -6,6 +6,52 @@ using System.Threading.Tasks;
 
 namespace RuleForge
 {
+    class TrpgStatus
+    {
+        public string StatusName { get; set; }
+        public int StatusValue { get; set; }
+
+        public TrpgStatus(string name, int value)
+        {
+            StatusName = name;
+            StatusValue = value;
+        }
+    }
+    class TrpgCommon
+    {
+        public Dictionary<string, TrpgStatus> Statuses { get; set; }
+
+        public TrpgCommon()
+        {
+            Statuses = new Dictionary<string, TrpgStatus>();
+        }
+
+        public void AddNewStatus(string name, int value)
+        {
+            if (!Statuses.ContainsKey(name))
+            {
+                Statuses[name] = new TrpgStatus(name, value);
+            }
+        }
+
+        public void UpdateStatus(string name, int value)
+        {
+            if (Statuses.ContainsKey(name))
+            {
+                Statuses[name].StatusValue = value;
+            }
+        }
+
+        public TrpgStatus GetStatus(string name)
+        {
+            if (Statuses.ContainsKey(name))
+            {
+                return Statuses[name];
+            }
+            return null;
+        }
+
+    }
 
 
 }
