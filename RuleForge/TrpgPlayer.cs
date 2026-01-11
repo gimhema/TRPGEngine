661d0849b5e947726d7c5a50816b10f4452adebf
kimmo <hemakim0215@naver.com>
2026-01-11 11:51:00 +0900

    .

--- RuleForge/TrpgPlayer.cs
+++ RuleForge/TrpgPlayer.cs
@@ -6,29 +6,5 @@ using System.Threading.Tasks;
 
 namespace RuleForge
 {
-    class PlayerBasicInfo
-    {
-        public int Id { get; set; }
-        public PlayerBasicInfo() { }
 
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-
-    class TrpgPlayer
-    {
-        public PlayerBasicInfo PlayerBasicInfo { get; set; }
-        public TrpgActorStatus PlayeStatus { get; set; }
-
-        public TrpgPlayer()
-        {
-            PlayerBasicInfo = new PlayerBasicInfo();
-        }
-
-        public void Init(PlayerBasicInfo playerBasicInfo, TrpgActorStatus playerStatus)
-        {
-            PlayerBasicInfo = playerBasicInfo;
-            PlayeStatus = playerStatus;
-        }
-    }
 }
