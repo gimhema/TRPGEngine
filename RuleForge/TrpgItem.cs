0c94435b6e5ee894bdbb77db1af141fd4583d4fb
kimmo <hemakim0215@naver.com>
2026-01-25 20:52:10 +0900

    .

--- RuleForge/TrpgItem.cs
+++ RuleForge/TrpgItem.cs
@@ -20,9 +20,19 @@ namespace RuleForge
 
     class Equipment : TrpgItem
     {
+        private Dictionary<string, TrpgStatus> EquipmentStatuses = new Dictionary<string, TrpgStatus>();
         public Equipment(string name, string description = "") : base(name, description)
         {
             
+        }
+
+        public TrpgStatus? GetStatusByName(string statusName)
+        {
+            if (EquipmentStatuses.ContainsKey(statusName))
+            {
+                return EquipmentStatuses[statusName];
+            }
+            return null;
         }
     }
 
@@ -31,6 +41,11 @@ namespace RuleForge
         public Consumable(string name, string description = "") : base(name, description)
         {
             
+        }
+
+        public void UseItem(TrpgActor targetActor)
+        {
+            Console.WriteLine($"{targetActor.Name} used {ItemName}.");
         }
     }
 
