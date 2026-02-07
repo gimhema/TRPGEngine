6f37f90c005f94310aa86827d92bca0f4831a87d
kimmo <hemakim0215@naver.com>
2026-02-07 12:36:27 +0900

    .

--- RuleForge/TrpgWorld.cs
+++ RuleForge/TrpgWorld.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,33 +10,86 @@ namespace RuleForge
 {
     public class WorldManager
     {
-        
+        public Dictionary<string, World> Worlds;
+
+        public WorldManager()
+        {
+            Worlds =  new Dictionary<string, World>();
+        }
+
+        public void LoadWorldInfoByRuleBook()
+        {
+            // Parse World Info from RuleBook . . .
+        }
+    }
+
+    public class WorldBasicInfo
+    {
+        public string Name {get; set;}
+        public string Description {get; set;}
+
+        public WorldBasicInfo()
+        {
+            Name = "Default";
+            Description = "Empty";
+        }
     }
 
     public class World
     {
-        
+        public WorldBasicInfo basicInfo;
+
+        public World()
+        {
+            basicInfo = new WorldBasicInfo();
+        }
+
+        public void SetWorldInfo(string _name, string _desc)
+        {
+            basicInfo.Name = _name;
+            basicInfo.Description = _desc;
+        }
+
+        public string GetName()
+        {
+            return basicInfo.Name;
+        }
+
+        public string GetDescription()
+        {
+            return basicInfo.Description;
+        }
         
     }
 
-    public class Village
+    public class WorldUnit
+    {
+        public WorldBasicInfo basicInfo;
+
+        public WorldUnit()
+        {
+            basicInfo = new WorldBasicInfo();
+        }
+    }
+
+    public class Village : WorldUnit
+    {
+
+
+    }
+
+    public class Establishment : WorldUnit
     {
         
-
-    }
-
-    public class Establishment
-    {
-        
         
     }
 
-    public class Field
+    public class Field : WorldUnit
     {
         
     }
 
-    public class Dungeon
+    public class Dungeon : WorldUnit
     {
         
 
