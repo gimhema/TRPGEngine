c07cb7f16a8a91fd01774e20506c136fb067c209
kimmo <hemakim0215@naver.com>
2026-02-07 12:04:50 +0900

    add world system

---
+++ RuleForge/TrpgWorld.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuleForge
+{
+    public class WorldManager
+    {
+        
+    }
+
+    public class World
+    {
+        
+        
+    }
+
+    public class Village
+    {
+        
+
+    }
+
+    public class Establishment
+    {
+        
+        
+    }
+
+    public class Field
+    {
+        
+    }
+
+    public class Dungeon
+    {
+        
+
+    }
+
+
+
+
+}
